887ba070b40e4c350bee4a5645961845c87ca524
Jamal Hashemi <Jamal Hashemi@DESKTOP-VVBDVAV>
2020-07-05 08:21:20 +0430

    Add Components folder and first app component

---
+++ gussing-nationality/src/components/country.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Country extends Component {
+  render() {
+    return <button class="btn btn-primary">Chinese</button>;
+  }
+}
+
+export default Country;
